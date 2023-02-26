1a9f186232081a8df1aa70fff7421d59dbde0be2
faridsolgi <solgifarid@gmail.com>
2023-02-26 18:20:34 +0330

    add some Constants and rename file from Consts.kt to Constants.kt

--- app/src/main/java/com/fsol/movieexample/Utils/Consts.kt
+++
@@ -1,5 +0,0 @@
-package com.fsol.movieexample.Utils
-
-object Consts {
-
-}
---
+++ app/src/main/java/com/fsol/movieexample/model/Utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.fsol.movieexample.model.Utils
+
+const val BASE_URL = "https://moviesapi.ir/api/v1/"
+const val CONNECTION_TIME = 60L
